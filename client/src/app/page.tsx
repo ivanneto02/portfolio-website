82f753b962daff9e31a854254c8f7d825eba6405
Ivan Neto <ivan.a.neto@hotmail.com>
2025-03-26 14:11:19 -0700

    Font change

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -2,10 +2,10 @@ import { Header, Footer, Portfolio } from "@/components";
 
 export default function Home() {
         return (
-                <div className="h-svh">
+                <div className="h-svh text-xl font-[family-name:var(--font-geist-sans)]">
                         <Header />
                         <Portfolio />
                         <Footer />
-                </div >
+                </div>
         );
 }
