9532a143ff4ea48d4ca1ec73bae1f8ed905e8d99
Ivan Neto <ivan.a.neto@hotmail.com>
2025-03-26 14:00:41 -0700

    Initial layout
    Header
    Portfolio
    Footer
    
    - Portfolio will contain list of projects, likely pages that will be
      defined with mdx.

---
+++ client/src/components/layout/Footer.tsx
@@ -0,0 +1,10 @@
+export function Footer() {
+        return (
+                <div className="flex justify-between h-[30%] bg-black pl-[10%] pr-[10%] pt-[3%]">
+                        <div>Item</div>
+                        <div>Item</div>
+                        <div>Item</div>
+                        <div>Item</div>
+                </div>
+        );
+}
---
+++ client/src/components/layout/Header.tsx
@@ -0,0 +1,10 @@
+export function Header() {
+        return (
+                <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-x1 bg-black h-[10%] pl-[10%] pr-[10%]">
+                        <div>Item</div>
+                        <div>Item</div>
+                        <div>Item</div>
+                        <div>Item</div>
+                </div>
+        );
+}
