5a748a962c752ea3a853a58c295c3333d3ea0e7f
ivanneto02 <ivan.a.neto@hotmail.com>
2025-03-31 23:58:41 -0700

    Improving responsiveness on mobile
    Header now works.

--- client/src/app/components/ui/Content.tsx
+++ client/src/app/components/ui/Content.tsx
@@ -10,7 +10,7 @@ export function Content({ children }: Props) {
 
         const { darkMode } = useContext(DarkModeContext);
 
-        const className = "dark:border-t-1 border-t-2 dark:shadow-md shadow-xs dark:bg-dark4 bg-white shadow-black dark:border-b-1 border-b-2 border-solid border-y-indigo1 min-h-screen content prose mt-[5%] pb-[5%] pl-[5%] pr-[5%] dark:prose-dark ml-[15%] mr-[15%] mb-[10%]";
+        const className = "dark:border-t-1 border-t-2 dark:shadow-md shadow-xs dark:bg-dark4 bg-white shadow-black dark:border-b-1 border-b-2 border-solid border-y-indigo1 min-h-screen content prose mt-[5%] pb-[5%] pl-[5%] pr-[5%] dark:prose-dark ml-[2%] mr-[2%] lg:ml-[15%] lg:mr-[15%] mb-[10%]";
 
         return (
                 <div className={darkMode ? className + " dark" : className}>
