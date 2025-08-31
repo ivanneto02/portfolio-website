4b57aa06dc49486388e0cbd7b90b6ba9f1489a3a
ivanneto02 <ivan.a.neto@hotmail.com>
2025-08-31 01:20:55 -0700

    Modifying look of project card, mobile
    adding better responsiveness to mobile devices

--- client/src/app/components/ui/ProjectCard.tsx
+++ client/src/app/components/ui/ProjectCard.tsx
@@ -11,16 +11,18 @@ interface PropsType {
 
 export function ProjectCard(props: PropsType) {
     return (
-        <a href={props.href} className="flex no-underline text-black dark:text-white flex-col gap-[3em] content-center lg:flex-row hover:bg-gray-200 dark:hover:bg-dark1 p-[2em] mb-[1em]">
-            <div className="lg:min-w-[2em] lg:max-w-[15em] lg:max-h-[15em]">
-                <Image className="w-[100%] h-[100%] lg:w-[100%] lg:h-[100%]" width={500} height={500} src={props.img_src} alt="" />
+        <a href={props.href} className="flex bg-gray-100 dark:bg-indigo-950 rounded items-center no-underline text-black flex-wrap lg:flex-nowrap dark:text-white flex-row lg:flex-col gap-[1em] lg:gap-[3em] lg:flex-row hover:bg-gray-200 dark:hover:bg-dark1 p-[1em] mb-[1em] lg:h-[100px]">
+            <div className="w-20 m-0 items-center lg:min-w-[2em] lg:max-w-[15em] lg:max-h-[15em]">
+                <Image className="w-[100%] lg:w-[100%] lg:h-[100%]" width={500} height={500} src={props.img_src} alt="" />
             </div>
-            <div>
-                <div className="flex flex-row gap-[1em] items-center">
-                    <h2 className="p-0 m-0">{props.title}</h2>
+            <div className="w-60 lg:w-100 m-0 self-start">
+                <div className="flex flex-col p-0 m-0 lg:flex-row gap-[1em] items-start lg:items-center">
+                    <h3 className="p-0 m-0">{props.title}</h3>
                     <p className="p-0 m-0">{props.date}</p>
                 </div>
-                <p>{props.description}</p>
+            </div>
+            <div className="w-100 m-0 lg:w-200 flex-stretch">
+                <p className="m-0 text-lg">{props.description}</p>
             </div>
         </a>
     );
